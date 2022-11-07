d3b9c910c31f0dba6f09bcd242bfefe7a0cb8069
Vincent Makokha <makokhavomondi@gmail.com>
2022-11-07 21:58:13 +0300

    Add more info to return value on get request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  # Get requests -------------------------------------------------------------------
   get '/categories' do 
-    Category.all.to_json(include: :houses)
+    Category.all.to_json(include: {
+      houses: {
+        only: [:id, :price, :description, :size_in_sqft, :image]
+      }
+    })
   end
 
   get '/categories/:id' do
@@ -11,12 +16,20 @@ class ApplicationController < Sinatra::Base
     if requested_category == []
       return {}.to_json
     else
-      return Category.find(params[:id]).to_json(include: :houses)
+      return Category.find(params[:id]).to_json(include: {
+        houses: {
+          only: [:id, :price, :description, :size_in_sqft, :image]
+        }
+      })
     end
   end
 
   get '/locations' do 
-    Location.all.to_json(include: :houses)
+    Location.all.to_json(include: {
+      houses: {
+        only: [:id, :price, :description, :size_in_sqft, :image]
+      }
+    })
   end
 
   get '/locations/:id' do
@@ -25,12 +38,30 @@ class ApplicationController < Sinatra::Base
     if requested_location == []
       return {}.to_json
     else
-      return Location.find(params[:id]).to_json(include: :houses)
+      return Location.find(params[:id]).to_json(include: {
+        houses: {
+          only: [:id, :price, :description, :size_in_sqft, :image]
+        }
+      })
     end
   end
 
   get '/houses' do
-    House.all.to_json(include: :categories)
+    requested_houses = House.all.map do |house|
+      {
+        id: house.id,
+        location_id: house.location_id,
+        category_id: house.category_id,
+        location: Location.find(house.location_id).location,
+        category: Category.find(house.category_id).category,
+        price: house.price,
+        description: house.description,
+        size_in_sqft: house.size_in_sqft,
+        image: house.image
+      }
+    end
+
+    requested_houses.to_json
   end
 
   get '/houses/:id' do
@@ -39,7 +70,18 @@ class ApplicationController < Sinatra::Base
     if requested_house == []
       return {}.to_json
     else
-      House.find(params[:id]).to_json
+      house = House.find(params[:id])
+      {
+        id: house.id,
+        location_id: house.location_id,
+        category_id: house.location_id,
+        location: Location.find(house.location_id).location,
+        category: Category.find(house.category_id).category,
+        price: house.price,
+        description: house.description,
+        size_in_sqft: house.size_in_sqft,
+        image: house.image
+      }.to_json
     end
   end
 end
